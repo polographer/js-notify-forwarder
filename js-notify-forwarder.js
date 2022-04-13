7738d380cd66f4be946342b2620e538ff5a86ab1
Polo Ornelas <m@poloornelas.mx>
2022-04-13 08:30:10 -0400

    initial version, it works, but its far from optimized, the code is ugly and there is not tests, this will change

--- js-notify-forwarder.js
+++ js-notify-forwarder.js
@@ -1,19 +1,62 @@
-var dbus = require('dbus-next')
-const bus = dbus.sessionBus();
+let dbus = require('dbus-next');
+const axios = require('axios').default;
+require('dotenv').config();
 
-bus.getProxyObject('org.freedesktop.DBus', '/org/freedesktop/DBus').then((obj) => {
-  let monitor = obj.getInterface('org.freedesktop.DBus.Monitoring');
+console.log("starting js-notify-forwarder");
+console.log("Pushover token: " + process.env['PUSHOVER_API_KEY']);
 
+var bus;
+var obj;
+var monitor;
+var skip = false;
+
+async function setup(){
+  bus = dbus.sessionBus();
+
+  obj = await bus.getProxyObject('org.freedesktop.DBus', '/org/freedesktop/DBus');
+  monitor = obj.getInterface('org.freedesktop.DBus.Monitoring');
   monitor.BecomeMonitor([
-    "type='signal',member='Notify',path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications'",
-    "type='method_call',member='Notify',path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications'",
-    "type='method_return',member='Notify',path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications'",
-    "type='error',member='Notify',path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications'"
-  ], 0);
+        "type='method_call',member='Notify',path='/org/freedesktop/Notifications',interface='org.freedesktop.Notifications'",
+      ], 0);
 
+  bus.on('message', parseAndSend);
+}
 
-  bus.on('message', (msg) => {
-    console.log(msg);
+function parseAndSend(msg){
+  if( msg.member && msg.member == 'Notify' ) {
+    var app = msg.body[0];
+    var summary = msg.body[3];
+    var body = msg.body[4];
+    body = body ? body.replace(/\n/g, " ") : "";
+    sendPushoverMessage(app, summary, body);
+    console.log(summary);
+    skip = false;
+  }
+}
+
+function sendPushoverMessage(app, summary, body) {
+  axios.post('https://api.pushover.net/1/messages.json', {
+    token: process.env['PUSHOVER_API_KEY'],
+    user: process.env['PUSHOVER_USER_KEY'],
+    title: "(" + app +") "+ summary,
+    message: body,
+    html: 1
+  })
+  .then(function (response) {
+    console.log("message sent");
+  })
+  .catch(function (error) {
+    console.log("error sending pushover message");
+    console.log(error);
   });
+}
 
-});
+async function main(){
+  if(!skip){
+    console.log( "setup done... ", await setup());
+    skip=true;
+  }
+}
+
+main();
+setInterval(main, 500);
